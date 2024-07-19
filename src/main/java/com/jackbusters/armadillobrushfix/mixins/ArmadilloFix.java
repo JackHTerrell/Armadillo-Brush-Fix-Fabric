64011ca6671b3997bb543c78e1ea672ae9bb841f
Jackbusters <77648717+JackHTerrell@users.noreply.github.com>
2024-07-19 03:40:39 -0500

    Fixed wrong argument used

--- src/main/java/com/jackbusters/armadillobrushfix/mixins/ArmadilloFix.java
+++ src/main/java/com/jackbusters/armadillobrushfix/mixins/ArmadilloFix.java
@@ -12,6 +12,6 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class ArmadilloFix {
     @Redirect(method = "mobInteract", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
     private boolean isInstanceOfBrush(ItemStack instance, Item item){
-        return item instanceof BrushItem;
+        return instance.getItem() instanceof BrushItem;
     }
 }
